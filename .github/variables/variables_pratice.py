7ed8187bd8a33c5465c301617ef40085612af7e0
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2021-10-29 14:44:24 -0400

    version 0.2
    printed() the three codes.

--- .github/variables/variables_pratice.py
+++ .github/variables/variables_pratice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Sophia Rentas, 10/29/21 2:37pm, version 0.1
+# Variables Practice, Sophia Rentas, 10/29/21 2:42pm, version 0.2
 
 # Three Common Types of Data
 
@@ -6,3 +6,7 @@ lives_remaining = 18 # INTEGERS, any +/- WHOLE nimber and 0.
 forty_time = 4.21 # FLOATING POINT, any +/- number witha decimal 
 death_cry = "AAAAAHHHHHHHHH!!!!"
 # Line 7 i a STRING -- collection of letters/numbers/others chars.
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
