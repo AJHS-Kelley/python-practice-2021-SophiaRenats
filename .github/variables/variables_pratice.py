1b656c9a1f5f3de02e5dfc093ab6b6de07d662a4
SophiaRenats <90347782+SophiaRenats@users.noreply.github.com>
2021-10-29 14:40:20 -0400

    version 0.1
    we descried an INTEGERS, FLOATING POINT, and STRING.

--- .github/variables/variables_pratice.py
+++ .github/variables/variables_pratice.py
@@ -1,1 +1,8 @@
-#Variables Practice, Sophia Rentas, 10/29/21 2:23pm, version 0.0
+# Variables Practice, Sophia Rentas, 10/29/21 2:37pm, version 0.1
+
+# Three Common Types of Data
+
+lives_remaining = 18 # INTEGERS, any +/- WHOLE nimber and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number witha decimal 
+death_cry = "AAAAAHHHHHHHHH!!!!"
+# Line 7 i a STRING -- collection of letters/numbers/others chars.
